7ff81f4c505ee588bd658ae3246511425b89908e
Paul Rowe <prowe214@gmail.com>
2019-10-02 11:43:03 -0600

    output responses at the end of the form

--- helpers/responseTemplates.js
+++ helpers/responseTemplates.js
@@ -4,31 +4,24 @@ module.exports = {
 
     buildQuestionResponseTemplate: function(index, oldForm) {
         let responseTemplate;
-        const question = questions[index];
-        console.log('QUESTION----------', index, question)
 
-        if (question.template_type === 'button') {
-            responseTemplate = {
-                attachment: {
-                    type: 'template',
-                    payload: {
-                        template_type: question.template_type,
-                        text: question.text,
-                        buttons:  question.options.map(item => {
-                            return {
-                                type: 'postback',
-                                title: item.title,
-                                // postback payload must be a string
-                                payload: this.saveAnswer(oldForm, question.field, item.value)
-                            }
-                        })
-                    }
-                }
+        if (questions[index]) {
+            const question = questions[index];
+            console.log('QUESTION----------', index, question)
+    
+            if (question.template_type === 'button') {
+                responseTemplate = this.buttonTemplate(question, oldForm);
+            } else if (question.template_type === 'quick_replies') {
+                responseTemplate = this.quickRepliesTemplate(question, oldForm);
+            } else {
+                responseTemplate = {text: 'I am not sure how to do that yet...'}
             }
-        } else if (question.template_type === 'quick_replies') {
-            responseTemplate = this.quickRepliesTemplate(question, oldForm);
         } else {
-            responseTemplate = {text: 'I am not sure how to do that yet...'}
+            let responses = Object.keys(oldForm).map(key => {
+                return `${key}: ${oldForm[key]}`
+            })
+            responses = responses.join(', ');
+            responseTemplate = { text: `You have completed the form!  Here are your responses: ${responses}` }
         }
 
         return responseTemplate;
@@ -56,6 +49,29 @@ module.exports = {
                 }
             })
         }
+
+        return responseTemplate;
+    },
+
+    buttonTemplate: function(question, oldForm) {
+        const responseTemplate = {
+            attachment: {
+                type: 'template',
+                payload: {
+                    template_type: question.template_type,
+                    text: question.text,
+                    buttons:  question.options.map(item => {
+                        return {
+                            type: 'postback',
+                            title: item.title,
+                            // postback payload must be a string
+                            payload: this.saveAnswer(oldForm, question.field, item.value)
+                        }
+                    })
+                }
+            }
+        }
+
         return responseTemplate;
     }
 }
